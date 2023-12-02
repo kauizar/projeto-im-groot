10888484e669142074cf9f08a42c74bd8f8eaf50
kauan izar <kauanizar02@gmail.com>
2023-12-02 18:17:48 -0300

    projeto groot

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿const string FraseParaSair = "tchau";
+Console.WriteLine("---- Groot chat -----\n");
+string pergunta;
+do {
+    Console.WriteLine("pergunta: ");
+    pergunta = Console.ReadLine()!;
+    string resposta = obtemrespostagroot(pergunta);
+    exiberesposta(obtemrespostagroot(pergunta));
+} 
+while (pergunta.Trim().ToLower() != FraseParaSair);
+void exiberesposta(string resposta)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($" Resposta: {resposta}");
+    Console.ResetColor();
+}
+string obtemrespostagroot(string pergunta)
+{
+    return pergunta.Trim().ToLower()== FraseParaSair ?
+    "Eu sou Groot!" :
+    "Eu sou Groot.";
+}
